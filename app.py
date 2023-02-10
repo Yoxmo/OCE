88479155aa85e02f888bf0219aa4e415ac5a80d8
YM <94254616+Yoxmo@users.noreply.github.com>
2023-02-10 14:14:18 -0500

    update

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ def list():
         newLoad.append(random.choice(txt))
     return render_template('list.html', newLoad=newLoad)
 
+@app.route('/view' , methods=['GET'])
+def view():
+    newLoad = []
+    for i in range(10):
+        newLoad.append(random.choice(txt))
+    return render_template('oce.html', newLoad=newLoad)
+
         
 @app.route('/list', methods=['POST']) 
 def listPost(): 
@@ -32,39 +39,41 @@ def listPost():
 
     for i in txt:
 
-        if(len(day) > 2 and len(time) > 2):
+        if(len(day) > 0 and len(time) > 0 and len(addit) > 0 ):
+            outputx = "[*] New search all time, addit, and day included..."
+
+            if str(day).lower() in str(i['Meeting_Time_and_Place']).lower() and str(time).lower() in str(i['Meeting_Time_and_Place']).lower() and str(addit).lower() in str(i).lower():
+                newLoad.append(i) 
+            else:
+                pass
+
+        elif(len(day) > 0 and len(time) > 0):
+            outputx = "[*] New search only day and time included..."
+
             if str(day).lower() in str(i['Meeting_Time_and_Place']).lower() and str(time).lower() in str(i['Meeting_Time_and_Place']).lower():
                 newLoad.append(i) 
             else:
                 pass
-        else:
-            pass 
 
-        if(len(time) > 2):
-            if str(time).lower() in str(i['Meeting_Time_and_Place']).lower():
+        elif(len(day) > 0 and len(addit) > 0):
+            outputx = "[*] New search only day and audit included..."
+
+            if str(day).lower() in str(i['Meeting_Time_and_Place']).lower() and str(addit).lower() in str(i).lower():
                 newLoad.append(i) 
             else:
                 pass
-        else:
-            pass 
 
-        if(len(day) > 2):
+        else:
+            outputx = "[*] New search only day included..."
+
             if str(day).lower() in str(i['Meeting_Time_and_Place']).lower():
                 newLoad.append(i) 
             else:
                 pass
-        else:
-            pass 
 
-        if(len(addit) > 2):
-            if str(addit).lower() in str(i).lower():
-                newLoad.append(i)   
-            else:
-                pass
-        else:
-            pass 
+    print(outputx)
 
-    return render_template('list.html', newLoad=newLoad)
+    return render_template('list.html', newLoad=newLoad, day= day, time = time, addit = addit)
 
 
 @app.route('/all')
